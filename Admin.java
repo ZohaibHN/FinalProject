53c4a9d35ef30d87ba07384757f5ec8c91f8291a
abroskie <adam@broskie.com>
2022-04-18 19:46:53 -0400

    Small progress (just to show that there's some being made)

--- Admin.java
+++ Admin.java
@@ -1,4 +1,10 @@
 import java.util.*;
+import java.io.*;
 public class Admin {
-
+    private String adminFirstName;
+    private String adminLastName;
+    private String adminAuthentificationNumber;
+    public Admin(String adminAuthentificationNumber) {
+        this.adminAuthentificationNumber = adminAuthentificationNumber;
+    }
 }
