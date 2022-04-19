52c048d7388c7f2d60ae0ec882b6bda6446cf967
Zohaib <zhassan@mail.umw.edu>
2022-04-19 03:46:03 -0400

    Made some methods for menu options as well as arraylist for employees in admin class

--- Admin.java
+++ Admin.java
@@ -1,9 +1,11 @@
 import java.util.*;
 import java.io.*;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 public class Admin {
     private static final String ADMIN_USERNAME = "p131759";
     private static final String ADMIN_PASSWORD = "UMW.42972";
+    private static ArrayList<Employee> employeeList = new ArrayList<Employee>();
     private int employeeRating;
 
     public static void createEmployee() {
@@ -13,7 +15,6 @@ public class Admin {
         String inputLName = scnr.nextLine();
         int inputID = scnr.nextInt();
 
-        Employee newEmployee = new Employee(inputFName, inputLName, inputID);
         System.out.println("Employee has been created");
 
     }
@@ -35,38 +36,60 @@ public class Admin {
             System.out.println("File Not Found");
         }
     }
+    public static void updateEmployeeList() {
+        try {
+            Scanner input = new Scanner(new File("EmployeeList"));
+            while (input.hasNextLine()) {
+                String firstName = input.nextLine();
+                String lastName = input.nextLine();
+                int employeeID = Integer.parseInt(input.nextLine());
+                Employee newEmployee = new Employee(firstName, lastName, employeeID);
+                employeeList.add(newEmployee);
+                input.nextLine();
+                input.nextLine();
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File Not Found");
+        }
+    }
 
     public void setRating() {
 
     }
     //public void menuOptions()
       public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.println("What would you like to do?");
-        System.out.println();
-        System.out.println("1. Create new employee.");
-        System.out.println("2. List employees.");
-        System.out.println("3. View Employee Reviews");
-        System.out.println("4. Rate Employees");
-        System.out.println("5. Exit");
-        int adminNum = input.nextInt();
-        switch (adminNum) {
-            case 1:
-                createEmployee();
-                break;
-            case 2:
-                listEmployees();
-                break;
-            case 3:
+          while (true) {
+              Scanner input = new Scanner(System.in);
+              System.out.println("What would you like to do?");
+              System.out.println();
+              System.out.println("1. Create new employee.");
+              System.out.println("2. List employees.");
+              System.out.println("3. Update Employee List");
+              System.out.println("4. View Employee Reviews");
+              System.out.println("5. Rate Employees");
+              System.out.println("6. Exit");
+              int adminNum = input.nextInt();
+              switch (adminNum) {
+                  case 1:
+                      createEmployee();
+                      break;
+                  case 2:
+                      listEmployees();
+                      break;
+                  case 3:
+                      updateEmployeeList();
+                      break;
+                  case 4:
 
-                break;
-            case 4:
+                      break;
+                  case 5:
 
-                break;
-            case 5:
-                System.exit(-1);
-        }
-    }
+                      break;
+                  case 6:
+                      System.exit(-1);
+              }
+          }
+      }
     public static String getAdminUsername() {
         return ADMIN_USERNAME;
     }
