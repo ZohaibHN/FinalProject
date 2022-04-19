6626897ec004643f1f1bba2efe2c4ecdf88eb8bf
abroskie <adam@broskie.com>
2022-04-18 22:59:53 -0400

    Various Progress

--- Customer.java
+++ Customer.java
@@ -1,4 +1,18 @@
 import java.util.*;
+import java.io.*;
 public class Customer {
+    public static void main(String args[]) throws IOException {
+        try(FileWriter fw = new FileWriter("Reviews.txt", true);
+            BufferedWriter bw = new BufferedWriter(fw);
+            PrintWriter out = new PrintWriter(bw))
+        {
+            out.println("the text");
+            //more code
+            out.println("more text");
+            //more code
+        } catch (IOException e) {
+            //exception handling left as an exercise for the reader
+        }
 
+    }
 }
