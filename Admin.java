08bdfc579a218fa0f909f31c0f70c55239bfc789
abroskie <adam@broskie.com>
2022-04-18 20:35:04 -0400

    Initial variable change

--- Admin.java
+++ Admin.java
@@ -2,7 +2,7 @@ import java.util.*;
 import java.io.*;
 public class Admin {
     private static final String ADMIN_USERNAME = "p131759";
-    private static final String ADMIN_PASSWORD = "Halo.42972";
+    private static final String ADMIN_PASSWORD = "UMW.42972";
     private int employeeRating;
 
 
