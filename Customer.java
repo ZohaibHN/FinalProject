4930a319fa9d1234da6b533fd47214e0c57a8f71
Zohaib <zhassan@mail.umw.edu>
2022-04-19 02:10:13 -0400

    Changes made to list out all employees and corresponding information

--- Customer.java
+++ Customer.java
@@ -3,10 +3,9 @@ import java.io.*;
 public class Customer {
     public void WriteReview() throws IOException {
         Scanner reviewInput = new Scanner(System.in);
-        try(FileWriter fw = new FileWriter("Reviews.txt", true);
-            BufferedWriter bw = new BufferedWriter(fw);
-            PrintWriter out = new PrintWriter(bw))
-        {
+        try (FileWriter fw = new FileWriter("Reviews.txt", true);
+             BufferedWriter bw = new BufferedWriter(fw);
+             PrintWriter out = new PrintWriter(bw)) {
             new FileWriter("Reviews.txt", false).close();
             out.println("Very nice attitude and helpful!");
             out.println("Always put on a smile.  Never seems like they are in a bad mood!");
@@ -20,13 +19,19 @@ public class Customer {
         }
 
     }
+
     public static void main(String[] args) throws FileNotFoundException {
         Scanner input = new Scanner(new File("EmployeeList"));
-
-        while(input.hasNext()) {
+        while (input.hasNextLine()) {
             String firstName = input.nextLine();
-
-            System.out.println(firstName);
+            String lastName = input.nextLine();
+            int employeeID = Integer.parseInt(input.nextLine());
+            input.nextLine();
+            input.nextLine();
+            //System.out.println(firstName);
+            //System.out.println(lastName);
+            System.out.println(employeeID);
+            System.out.println();
         }
     }
 }
