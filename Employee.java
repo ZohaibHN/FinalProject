5cb8a2a504b505a4230bf6be4bc245687b32d38d
abroskie <adam@broskie.com>
2022-04-18 20:08:46 -0400

    Small progress (just to show that there's some being made)

--- Employee.java
+++ Employee.java
@@ -3,7 +3,7 @@ public class Employee {
     private String employeeFName;
     private String employeeLName;
     private String employeeID;
-
+    private ArrayList<String> ratings;
     public Employee(String employeeFName, String employeeLName, String employeeID) {
         this.employeeFName = employeeFName;
         this.employeeLName = employeeLName;
