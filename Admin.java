d0334115a1b86ccc4df99472b1c76a19c9228775
abroskie <adam@broskie.com>
2022-04-18 20:03:29 -0400

    Small progress (just to show that there's some being made)

--- Admin.java
+++ Admin.java
@@ -16,4 +16,5 @@ public class Admin {
     public void setRating() {
 
     }
+
 }
--- Employee.java
+++ Employee.java
@@ -1,4 +1,12 @@
 import java.util.*;
 public class Employee {
+    private String employeeFName;
+    private String employeeLName;
+    private String employeeID;
 
+    public Employee(String employeeFName, String employeeLName, String employeeID) {
+        this.employeeFName = employeeFName;
+        this.employeeLName = employeeLName;
+        this.employeeID = employeeID;
+    }
 }
