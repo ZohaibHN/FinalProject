c96e4bfb4878031d52d8fd0ddc2a1a9bf446369d
jamanuel <94139585+jamanuel@users.noreply.github.com>
2022-04-19 20:49:07 -0700

    Refactored reading employees from file

--- Employee.java
+++ Employee.java
@@ -40,5 +40,24 @@ public class Employee  {
     public void addRating(String rating) {
         ratings.add(rating);
     }
+    
+    public static ArrayList<Employee> getEmployeesFromFile(String file) throws FileNotFoundException {
+        Scanner sc = new Scanner(file); 
+        ArrayList<Employee> e = new ArrayList<Employee>(); 
+        
+        while(sc.hasNextLine()) {
+            String fn = sc.nextLine(); 
+            String ln = sc.nextLine(); 
+            int id = Integer.parseInt(sc.nextLine()); 
+            int rating = Integer.parseInt(sc.nextLine()); 
+            Employee temp = new Employee(fn, ln, id, rating); 
+            
+            int numRevs = Integer.parseInt(sc.nextLine()); 
+            for(int i = 0; i < numRevs; i++) 
+                temp.addRating(sc.nextLine()); 
+        }
+        
+        return e; 
+    } 
 
 }
