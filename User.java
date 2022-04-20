9818718f93d12b7c5d01cc2e24991b83149a8aa5
abroskie <adam@broskie.com>
2022-04-20 00:37:14 -0400

    commit

--- User.java
+++ User.java
@@ -51,109 +51,109 @@ public class User {
     }
     public static void listEmployees() {
         try {
-            ArrayList<Employees> employees = Employee.getEmployeesFromFile("EmployeeList"); 
+            ArrayList<Employee> employees = Employee.getEmployeesFromFile("EmployeeList");
             for(Employee e : employees) {
-                System.out.printf("%s %s\n\tID: %d\n\trating: %d\n\n", 
-                                  e.getEmployeeFName(), e.getEmployeeLName(), 
-                                  e.getEmployeeID(), e.getRating()); 
+                System.out.printf("%s %s\n\tID: %d\n\trating: %d\n\n",
+                        e.getEmployeeFName(), e.getEmployeeLName(),
+                        e.getEmployeeID(), e.getRating());
                 pause(0.25D);
-            } 
+            }
             pause(0.66D);
         }  catch (FileNotFoundException e) {
             System.out.println("File Not Found");
-            System.exit(-1); 
+            System.exit(-1);
         }
     }
 
 
-    public static void rateEmployee() throws FileNotFoundException {
+    public static void rateEmployee() throws IOException {
         int i = 1;
         Scanner input = new Scanner(System.in);
-        
-        ArrayList<Employee> employees = Employee.getEmployeesFromFile("EmployeeList"); 
-        for(Employee e : employees) 
-            System.out.printf("[%d] %s %s\n", i++, e.getEmployeeFName(), e.getEmployeeLName()); 
-        
+
+        ArrayList<Employee> employees = Employee.getEmployeesFromFile("EmployeeList");
+        for(Employee e : employees)
+            System.out.printf("[%d] %s %s\n", i++, e.getEmployeeFName(), e.getEmployeeLName());
+
         System.out.println("Which employee would you like to select?");
         int numSelection = input.nextInt();
-        
+
         while(numSelection < 1 || numSelection > employees.size()) {
-            System.out.println("That's not a valid employee."); 
+            System.out.println("That's not a valid employee.");
             System.out.println("Which employee would you like to select?");
             numSelection = input.nextInt();
         }
-        
-        System.out.println("What rating would you like to give them? [0-5]"); 
-        int rating = input.nextInt(); 
+
+        System.out.println("What rating would you like to give them? [0-5]");
+        int rating = input.nextInt();
         while(rating < 0 || rating > 5) {
-            System.out.println("That's not a valid star rating."); 
-            System.out.println("What rating would you like to give them? [0-5]"); 
-            rating = input.nextInt(); 
+            System.out.println("That's not a valid star rating.");
+            System.out.println("What rating would you like to give them? [0-5]");
+            rating = input.nextInt();
         }
-        
-        employees.get(numSelection - 1).setStarRating(rating); 
-        
-        // writing out the data back to the original file. 
-        Employee.writeToFile("EmployeeList", employees); 
+
+        employees.get(numSelection - 1).setStarRating(rating);
+
+        // writing out the data back to the original file.
+        Employee.writeToFile("EmployeeList", employees);
     }
 
-    public static void readEmployeeReviews(int ID) {
-        ArrayList<Employee> employees = Employee.getEmployeesFromFile("EmployeeList"); 
+    public static void readEmployeeReviews(int ID) throws FileNotFoundException {
+        ArrayList<Employee> employees = Employee.getEmployeesFromFile("EmployeeList");
         for(Employee e : employees) {
             if(e.getEmployeeID() == ID) {
-                System.out.printf("Customer reviews for employee #%d:\n", ID); 
+                System.out.printf("Customer reviews for employee #%d:\n", ID);
                 for(String review : e.getReviews()) {
-                    System.out.println(review); 
-                    System.out.println(); 
-                } 
+                    System.out.println(review);
+                    System.out.println();
+                }
             }
-        } 
-        
-        System.out.printf("The provided ID (%d) was not found.\n", ID); 
+        }
+
+        System.out.printf("The provided ID (%d) was not found.\n", ID);
     }
 
-      public static void adminMenu() throws IOException {
-          while (true) {
-              Scanner input = new Scanner(System.in);
-              System.out.println("What would you like to do?");
-              pause(0.5D);
-              System.out.print(".");
-              pause(0.5D);
-              System.out.print(".");
-              pause(0.5D);
-              System.out.println();
-              System.out.println("1. Create new employee.");
-              System.out.println("2. List employees.");
-              System.out.println("3. View Employee Reviews");
-              System.out.println("4. Rate Employees");
-              System.out.println("5. Exit");
-              int adminNum = input.nextInt();
-              switch (adminNum) {
-                  case 1 -> createEmployee();
-                  case 2 -> listEmployees();
-                  case 3 -> readEmployeeReviews(0);
-                  case 4 -> rateEmployee(); //rateEmployees();
-                  case 5 -> System.exit(-1);
-              }
-          }
-      }
-      public static void employeeMenu() {
-        Scanner input = new Scanner(System.in);
-            System.out.println("1. View Employee Reviews");
-            System.out.println("2. Exit");
-            System.out.print("What would you like to do?: ");
-            int userInput = input.nextInt();
-            switch(userInput) {
-                case 1:
-                    System.out.print("Enter Employee ID: ");
-                    int employeeID = input.nextInt();
-                    readEmployeeReviews(employeeID);
-                    break;
-                case 2:
-                    System.exit(-1);
-                    break;
+    public static void adminMenu() throws IOException {
+        while (true) {
+            Scanner input = new Scanner(System.in);
+            System.out.println("What would you like to do?");
+            pause(0.5D);
+            System.out.print(".");
+            pause(0.5D);
+            System.out.print(".");
+            pause(0.5D);
+            System.out.println();
+            System.out.println("1. Create new employee.");
+            System.out.println("2. List employees.");
+            System.out.println("3. View Employee Reviews");
+            System.out.println("4. Rate Employees");
+            System.out.println("5. Exit");
+            int adminNum = input.nextInt();
+            switch (adminNum) {
+                case 1 -> createEmployee();
+                case 2 -> listEmployees();
+                case 3 -> readEmployeeReviews(0);
+                case 4 -> rateEmployee(); //rateEmployees();
+                case 5 -> System.exit(-1);
             }
+        }
+    }
+    public static void employeeMenu() throws FileNotFoundException {
+        Scanner input = new Scanner(System.in);
+        System.out.println("1. View Employee Reviews");
+        System.out.println("2. Exit");
+        System.out.print("What would you like to do?: ");
+        int userInput = input.nextInt();
+        switch(userInput) {
+            case 1:
+                System.out.print("Enter Employee ID: ");
+                int employeeID = input.nextInt();
+                readEmployeeReviews(employeeID);
+                break;
+            case 2:
+                System.exit(-1);
+                break;
+        }
 
 
-      }
+    }
 }
